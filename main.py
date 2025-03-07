9a17fd95270e908646d24343b3fe9017027bf99f
Enkhamgalan1230 <115167095+Enkhamgalan1230@users.noreply.github.com>
2025-03-07 21:01:11 +0000

    sql

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
 import streamlit as st
+import mysql.connector
+import pandas as pd
 
-st.write("Hello World 👋")
+st.title("Hello World 👋")
+
+connection = mysql.connector.connect(
+    host = '127.0.0.1:3306',
+    user = 'root',
+    password = 'lkjH0987@',
+    database = 'products'
+)
+
+print('connected')
+
+cursor = connection.cursor()
+
+cursor.execute("SELECT * FROM products.product")
+data = cursor.fetchall()
+
+df = pd.DataFrame(data, columns=cursor.column_names)
+st.dataframe(df)
