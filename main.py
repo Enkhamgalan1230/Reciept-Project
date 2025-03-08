aa74d0970923eb319663d375b3ae365246cb713b
Enkhamgalan1230 <115167095+Enkhamgalan1230@users.noreply.github.com>
2025-03-08 01:27:24 +0000

    update

--- main.py
+++ main.py
@@ -2,15 +2,19 @@ import streamlit as st
 import mysql.connector
 import pandas as pd
 from sqlalchemy import create_engine
+from st_supabase_connection import SupabaseConnection
 
 st.title("Hello World 👋")
 
 # Initialize connection
-conn = st.connection('supabase')
+conn = st.connection("supabase", type=SupabaseConnection)
 
-# Query your Supabase database
-df = conn.query('SELECT * FROM mytable;')
+# Perform query
+rows = conn.query("*", table="mytable", ttl="10m").execute()
 
-# Display results
-st.dataframe(df)
-
+# Print results
+if rows.data:
+    for row in rows.data:
+        st.write(f"{row['name']} has a :{row['pet']}:")
+else:
+    st.write("No data found!")
