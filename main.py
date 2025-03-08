016e1b71042b6b072e5ea434e94b86c42c201068
Enkhamgalan1230 <115167095+Enkhamgalan1230@users.noreply.github.com>
2025-03-08 01:40:38 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import mysql.connector
 import pandas as pd
-from sqlalchemy import create_engine
 from st_supabase_connection import SupabaseConnection
 from supabase import create_client, Client
 
