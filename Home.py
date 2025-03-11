467caf6c8c6971059af961aa7333a10ffa14a31a
Enkhamgalan1230 <115167095+Enkhamgalan1230@users.noreply.github.com>
2025-03-11 19:21:40 +0000

    logo changes

--- Home.py
+++ Home.py
@@ -22,7 +22,7 @@ col1, col2 = st.columns([1, 2], vertical_alignment="center", gap= "small")  # Ad
 
 # Left Column: Image
 with col1:
-    st.image("page.png") 
+    st.image("logo.png") 
     
 # Right Column: Text
 with col2:
