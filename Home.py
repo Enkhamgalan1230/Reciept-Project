53d69b31840a0f5cd391c252122ce082bd3fa2be
Enkhamgalan1230 <115167095+Enkhamgalan1230@users.noreply.github.com>
2025-03-11 20:00:02 +0000

    Update Home.py

--- Home.py
+++ Home.py
@@ -51,7 +51,7 @@ pg = st.navigation(
         "Project":[data, page_3,page_4,page_5]
     }
 )
-st.logo("assets/logo_longer.png",icon_image="assets/logo.png")
+st.logo("assets/logo_longer.png",icon_image="assets/logo.png", size= "large")
 st.sidebar.text("Made by Entwan Enkhbayar")
 
 pg.run()
