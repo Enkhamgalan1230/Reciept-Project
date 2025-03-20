ec8c95473359c5ef732a7adfed4f68cc913ea762
Enkhamgalan1230 <115167095+Enkhamgalan1230@users.noreply.github.com>
2025-03-20 17:31:53 +0000

    Update 7_reciept.py

--- pages/7_reciept.py
+++ pages/7_reciept.py
@@ -5,7 +5,7 @@ from geopy.distance import geodesic
 from streamlit_geolocation import streamlit_geolocation
 from streamlit_js_eval import streamlit_js_eval, copy_to_clipboard, create_share_link, get_geolocation
 
-def get_store_locations_photon(store_name, user_lat, user_lon, max_distance_km):
+def get_store_locations(store_name, user_lat, user_lon, max_distance_km):
     url = "https://photon.komoot.io/api/"
     params = {
         "q": store_name,  
