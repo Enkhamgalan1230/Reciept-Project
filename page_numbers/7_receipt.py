5bbfca989402a0aaf885b2521e183da99d0f2691
Enkhamgalan1230 <115167095+Enkhamgalan1230@users.noreply.github.com>
2025-03-24 18:58:17 +0000

    Update 7_receipt.py

--- page_numbers/7_receipt.py
+++ page_numbers/7_receipt.py
@@ -179,7 +179,7 @@ with container:
 
     voice_products = []
 
-    if len(audio) > 0:
+    if audio is not None and len(audio) > 0:
         st.audio(audio, format="audio/wav")
 
         with tempfile.NamedTemporaryFile(delete=False, suffix=".wav") as f:
