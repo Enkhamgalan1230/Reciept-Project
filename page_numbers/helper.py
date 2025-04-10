368a54828d6abbb86fc0847413a3b71165ae0029
Enkhamgalan1230 <115167095+Enkhamgalan1230@users.noreply.github.com>
2025-04-10 16:44:53 +0100

    Image size with columns

--- page_numbers/helper.py
+++ page_numbers/helper.py
@@ -6,8 +6,11 @@ st.markdown("---")
 st.subheader("FAQ", anchor=False)
 
 with st.expander("💡 Hint: No data available"):
-    st.image("assets/hint.png", width=200)
+    col1, col2, col3 = st.columns([1, 2, 1])  # middle column is 2x wider
+    with col2:
+        st.image("assets/hint.png", use_column_width=True)
+
     st.markdown(
         "Do not worry, we store the data in cache so you can simply visit the "
-        "[Data Fetcher](./data_fetcher) page and come back to that page."
+        "[Data Fetcher](./data_fetcher) page."
     )
