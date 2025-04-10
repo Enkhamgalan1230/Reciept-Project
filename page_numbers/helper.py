29ddb8da33238661e679c22ea0f011fe53a95a79
Enkhamgalan1230 <115167095+Enkhamgalan1230@users.noreply.github.com>
2025-04-10 16:42:50 +0100

    Update helper.py

--- page_numbers/helper.py
+++ page_numbers/helper.py
@@ -6,8 +6,8 @@ st.markdown("---")
 st.subheader("FAQ", anchor=False)
 
 with st.expander("💡 Hint: No data available"):
-    st.image("assets/hint.png", caption="Cached data hint", use_container_width=True)
+    st.image("assets/hint.png", use_container_width=True)
     st.markdown(
         "Do not worry, we store the data in cache so you can simply visit the "
-        "[Data Fetcher](./data_fetcher) page."
+        "[Data Fetcher](./data_fetcher) page and come back to that page."
     )
