6504bce082c69babd7c58a5b70a65fe05013e893
Enkhamgalan1230 <115167095+Enkhamgalan1230@users.noreply.github.com>
2025-04-15 16:26:30 +0100

    Update 8_receipt.py

--- page_numbers/8_receipt.py
+++ page_numbers/8_receipt.py
@@ -423,7 +423,7 @@ latest_date = df["Date"].max()
 latest_df = df[df["Date"] == latest_date]
 
 st.write(latest_df)
-st.write(latest_df[latest_df["Store_Name"] == "Aldi"])
+st.write(latest_df[latest_df["Store_Name"] == "Sainsburys"])
 
 options = ["Tesco", "Waitrose", "Asda", "Aldi", "Sainsburys"]
 selection = st.pills("Stores", options, selection_mode="single")
