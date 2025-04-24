2b9b31f44363b4a681a04da75fded807a701b68b
Enkhamgalan1230 <115167095+Enkhamgalan1230@users.noreply.github.com>
2025-04-24 22:40:57 +0100

    Update 6_price_prediction.py

--- page_numbers/6_price_prediction.py
+++ page_numbers/6_price_prediction.py
@@ -22,7 +22,7 @@ df_melted = cpih.melt(id_vars="Product", var_name="Date", value_name="Index")
 df_melted["Date"] = pd.to_datetime(df_melted["Date"], format="%Y %b")
 
 # Streamlit caption for context
-st.caption("📌 CPIH Index is relative to the base year (e.g., 2015 = 100). If a product is at 150, it means prices have risen 50% since 2015.")
+
 
 # Create dropdown to select products
 all_products = df_melted["Product"].unique().tolist()
@@ -43,9 +43,12 @@ if selected:
     fig.update_layout(
         xaxis_title="Date",
         yaxis_title="CPIH Index",
-        xaxis_tickangle=-45  # 👈 Rotate labels
+        xaxis_tickangle=-45,
+        xaxis=dict(tickformat="%b %Y")  # e.g. "Apr 2024"
     )
 
     st.plotly_chart(fig, use_container_width=True)
 else:
     st.info("Please select at least one product category to display the chart.")
+
+st.caption("📌 CPIH Index is relative to the base year (e.g., 2015 = 100). If a product is at 150, it means prices have risen 50% since 2015.")
