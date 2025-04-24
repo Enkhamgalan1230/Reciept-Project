27bf07e2dd270618b848853939a30671356f93f2
Enkhamgalan1230 <115167095+Enkhamgalan1230@users.noreply.github.com>
2025-04-25 00:53:43 +0100

    Update 6_price_prediction.py

--- page_numbers/6_price_prediction.py
+++ page_numbers/6_price_prediction.py
@@ -10,7 +10,6 @@ st.caption("💡 Historical data is provided by ONS(Office for National Statisti
 # Check if df is stored in session state
 if "df" in st.session_state:
     df = st.session_state.df  # Retrieve stored data
-    st.write("Nothing here yet!")
 else:
     st.warning("💡 Hint: No data available. Please visit the Data Fetcher page quickly and come back to this page.")
 
