932ed446d90dbc3cf98e99b791f092615dca8a8e
Enkhamgalan1230 <115167095+Enkhamgalan1230@users.noreply.github.com>
2025-07-14 08:57:28 +0100

    Update data_fetcher.py

--- page_numbers/data_fetcher.py
+++ page_numbers/data_fetcher.py
@@ -101,7 +101,7 @@ if "df" not in st.session_state:
             st.write(f"There are {max_rows} rows currently in the database.")
 
             # Show an initial fun message
-            st.info("Loading fresh data from Supabase! 🍽️ Since we fetch 1,000 rows at a time, it may take a moment. Why not grab a coffee and enjoy a fun fact while you wait? ", icon="ℹ☕")
+            st.info("Loading fresh data from Supabase! 🍽️ Since we fetch 1,000 rows at a time, it may take a moment. Why not grab a coffee and enjoy a fun fact while you wait? ", icon="☕")
 
             # Step 2: Fetch data with pagination
             batch_size = 1000
